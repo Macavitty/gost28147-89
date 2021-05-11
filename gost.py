103f8206548cd89f8e68512922372efd3b4907ed
Macavitty <somemail@mail.com>
2021-05-11 23:51:15 +0300

    for report

--- gost.py
+++ gost.py
@@ -1,6 +1,6 @@
 import math
 
-# статичный блок подстановок
+# S-boxes used by the Central Bank of RF
 s_box = (
     (4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3),
     (14, 11, 4, 12, 6, 13, 15, 10, 2, 3, 8, 1, 0, 7, 5, 9),
@@ -13,14 +13,32 @@ s_box = (
 )
 
 BLOCK_SIZE_BYTES = 8
+BLOCK_SIZE_BITS = BLOCK_SIZE_BYTES * 8
+KEY_SIZE_BYTES = 32
+IV_SIZE_BYTES = 8
 
 
+### Counts the number of bits needed to represent the number.
+# @type n:   int
+# @param n:  number
+# @rtype:    int
+# @return:   the number of bits for n
+###
 def get_num_bits(n):
     if n == 0:
         return 0
     return int((math.log(n) / math.log(2)) + 1)
 
 
+### Converts string to its int representation.
+# For each character as utf-8 gets its bits
+# representation and forms a number
+#
+# @type s:      str
+# @param s:     string to be converted
+# @rtype:       int
+# @return:      int representation of s
+###
 def str_as_int(s):
     if s == '':
         return 0
@@ -31,20 +49,36 @@ def str_as_int(s):
     return num
 
 
+### Converts integer to its string representation.
+# For each 8 bits gets its utf-8
+# representation and forms a string
+
+# @type num:      int
+# @param num:     integer to be converted
+# @rtype:         str
+# @return:        string representation of num
+###
 def int_as_str(num):
     s = ''
     mask = 0xff
     i = 0
     l = num.bit_length()
+    # starting from the lower bits
+    # in order nit to miss leading zeros
     while i < l:
         s += chr(num & mask)
         num >>= 8
         i += 8
-    s = s[::-1]
-    return s
+    return s[::-1]  # reversed string
 
 
-def make_block_string(block):
+### Fills block with trailing zeros if it doesn't fit BLOCK_SIZE_BITS.
+# @type block:   str
+# @param block:  block to be filled
+# @rtype:        str
+# @return:       formatted block
+###
+def make_block(block):
     if isinstance(block, str):
         block_bytes = len(block)
         needed_bits = int(BLOCK_SIZE_BYTES - block_bytes) * 8
@@ -53,25 +87,43 @@ def make_block_string(block):
     raise ValueError('expected str')
 
 
-def f(part, key):
+### The f function described in the gost.
+# @type part:       int
+# @param part:      left or right part of the block
+# @type subkey:     int
+# @param subkey:    subkey for current iteration
+# @rtype:           int
+# @return:          result of f
+###
+def f(part, subkey):
     mod32 = 0x100000000
-    # суммирование по модулю 2^32
-    part += key
+
+    # sum modulo 2^32
+    part += subkey
     if part >= mod32:
         part -= mod32
 
-    tmp = part
+    # performing substitution with s-boxes nodes
     mask = 0xf0000000
     res = 0
     for i in range(8):
-        ind = (tmp & mask) >> 28
-        tmp <<= 4
+        ind = (part & mask) >> 28
+        part <<= 4
         res <<= 4
         res |= s_box[i][ind]
+    # result is shifted left by 11 bits
     return ((res >> (32 - 11)) | (res << 11)) & 0xffffffff
 
 
-def generate_gamma(block, sub_keys):
+### Implementation of the Feistel cipher .
+# @type block:      str
+# @param block:     block to be ciphered
+# @type sub_keys:   list
+# @param sub_keys:  list of sub keys ordered for operation (encryption/decryption)
+# @rtype:           int
+# @return:          computed gamma
+###
+def compute_gamma(block, sub_keys):
     left = block >> 32
     right = block & 0xffffffff
 
@@ -82,11 +134,21 @@ def generate_gamma(block, sub_keys):
     return (left << 32) | right
 
 
+### Creates subkeys from the key.
+# Subkeys would be ordered for encryption like following:
+# k0, k1, k2, k3, k4, k5, k6, k7,  k0, k1, k2, k3, k4, k5, k6, k7,
+# k0, k1, k2, k3, k4, k5, k6, k7,  k7, k6, k5, k4, k3, k2, k1, k0
+
+# @type key:   str
+# @param key:  the key given by user
+# @rtype:      list
+# @return:     list of sub_keys
+###
 def get_sub_keys(key):
     key_list = []
     sub_keys = []
     i = 0
-    while i < 32:
+    while i < KEY_SIZE_BYTES:
         key_list.append(str_as_int(key[i: i + 4]))
         sub_keys.insert(0, key_list[len(key_list) - 1])
         i += 4
@@ -97,29 +159,42 @@ def get_sub_keys(key):
     return sub_keys
 
 
+### Performs encryption or decryption.
+# @type source:     str
+# @param source:    open or encrypted text, provided by user
+# @type key:        str
+# @param key:       the key, provided by user
+# @type iv:         str
+# @param iv:        "синхропосылка" - starting gamma, provided by user
+# @type operation:  str
+# @param operation: operation to perform must be "enc" or "dec"
+# @rtype:           str
+# @return:          encrypted or decrypted text
+###
 def gost(source, key, iv, operation='enc'):
-    assert len(key) == 32, 'Your key is ' + str(len(key)) + ' bytes but must be 32 bytes'
-    assert len(iv) == 8, 'iv must be 8 bytes'
-    assert operation == 'enc' or operation == 'dec', 'Unsupported operation: ' + operation
+    assert len(key) == KEY_SIZE_BYTES, f'Your key is {len(key)} bytes but must be {KEY_SIZE_BYTES} bytes'
+    assert len(iv) == IV_SIZE_BYTES, f'iv must be {IV_SIZE_BYTES} bytes'
+    assert operation == 'enc' or operation == 'dec', f'Unsupported operation: {operation}'
 
     sub_keys = get_sub_keys(key)
     # if operation == 'dec':
     #     sub_keys.reverse()
 
-    crypted = 0
+    result = 0
     i = 0
 
-    gamma = generate_gamma(str_as_int(iv), sub_keys)
-    gamma ^= make_block_string(source[i:min(i + 8, len(source))])
-    crypted |= gamma
-    i += 8
-    while i < len(source):
-        gamma = generate_gamma(gamma, sub_keys)
-        gamma ^= make_block_string(source[i:min(i + 8, len(source))])
-        crypted <<= 64
-        crypted |= gamma
-        i += 8
+    # first iteration with iv
+    gamma = compute_gamma(str_as_int(iv), sub_keys)
+    gamma ^= make_block(source[i:min(i + BLOCK_SIZE_BYTES, len(source))])
+    result |= gamma
 
-    print(int_as_str(crypted))
-    print(crypted)
-    return int_as_str(crypted)
+    # for other part of text
+    i += BLOCK_SIZE_BYTES
+    while i < len(source):
+        gamma = compute_gamma(gamma, sub_keys)
+        gamma ^= make_block(source[i:min(i + BLOCK_SIZE_BYTES, len(source))])
+        result <<= BLOCK_SIZE_BITS
+        result |= gamma
+        i += BLOCK_SIZE_BYTES
+
+    return int_as_str(result)
--- main.py
+++ main.py
@@ -1,28 +1,95 @@
+import filecmp
+import os
+import sys
+from ggost import *
 from gost import *
-import uuid
 
 
+### Reads file safely.
+# @type path:   str
+# @param path:  the path to file to be read
+# @rtype:       str or None
+# @return:      file content if file exists and not empty, None otherwise
+###
 def read(path):
+    if not os.path.exists(path):
+        print(f'No such file: "{path}"')
+        return None
+
     f = open(path, 'r')
     content = f.read()
     f.close()
+
+    if content == '':
+        print(f'File "{path}" is empty')
+        return None
+
     return content
 
 
-def write(path, content):
+### Rewrites file safely.
+# @type path:       str
+# @param path:      the path to file to be rewritten
+# @type content:    str
+# @param content:   content to be written
+###
+def writе(path, content):
     f = open(path, 'w')
     f.write(str(content))
     f.close()
 
 
+### Compares two files.
+# @type f1:     str
+# @param f1:    the first file to be compared
+# @type f2:     str
+# @param f2:    the second file to be compared
+# @rtype:       boolean
+# @return:      True if files contents are equal, False otherwise
+###
+def compare(f1, f2):
+    return filecmp.cmp(f1, f2)
+
+
+### Example of the gost28147-89 usage.
+# Used files:
+# file "source"         contains open text
+# file "key"            contains 32-byte key
+# file "iv"             contains 8-byte iv (named as "синхропосылка" in the gost)
+# file "encrypted"      contains encrypted text
+# file "decrypted"      contains decrypted text
+###
 if __name__ == '__main__':
-    source = read('source')
-    key = read('key')
+    encrypted_file = 'encrypted'
+    decrypted_file = 'decrypted'
+    source_file = 'source'
+    key_file = 'key'
+    iv_file = 'iv'
 
-    # starting_gamma = uuid.uuid1().int >> 64
-    iv = read('iv')
-    print(source)
+    source = read(source_file)
+    if source is None:
+        sys.exit(2)
+
+    key = read(key_file)
+    if key is None:
+        sys.exit(2)
+
+    iv = read(iv_file)
+    if iv is None:
+        sys.exit(2)
+
+    print(f'Encrypting file "{source_file}"...')
     write('encrypted', gost(source, key, iv, operation='enc'))
-    encrypted = read('encrypted')
-    write('decrypted', gost(encrypted, key, iv, operation='dec'))
+    print(f'Encryption completed successfully: result in file "{encrypted_file}"')
 
+    encrypted = read('encrypted')
+    if encrypted is None:
+        print(f'Could not perform further decryption due to error above')
+        sys.exit(2)
+
+    print(f'Decrypting file "encrypted"')
+    write('decrypted', gost(encrypted, key, iv, operation='dec'))
+    if compare(source_file, decrypted_file):
+        print(f'Decrypted successfully: files "{source_file}" and "{decrypted_file}" are equal')
+    else:
+        print(f'Decryption failed: files "{source_file}" and "{decrypted_file}" differ')
